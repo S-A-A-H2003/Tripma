5439f81e215aa4e98370b5ed73c8166f0fe0055e
saeedAZ19 <abuhalimasaeed2@gmail.com>
2024-12-12 14:02:56 +0200

    update-main.tsx-async

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { worker } from './mocks/browser'
- 
-await worker.start()
+
+async function startWorker() {
+  await worker.start();
+}
+startWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
